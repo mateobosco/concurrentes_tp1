7fda2a46a10c638d31e83579148c91eb83edbbcc
Juan Hidalgo <juan.hidalgo@fdvsolutions.com>
2015-05-06 00:23:25 -0300

    Loggeos y checkeo de errores

---
+++ src/ZappiFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by juan on 05/05/15.
+//
+
+#include "ZappiFactory.h"
---
+++ src/ZappiFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by juan on 05/05/15.
+//
+
+#ifndef CONCURRENTES_TP1_ZAPPIFACTORY_H
+#define CONCURRENTES_TP1_ZAPPIFACTORY_H
+
+
+class ZappiFactory {
+
+};
+
+
+#endif //CONCURRENTES_TP1_ZAPPIFACTORY_H
